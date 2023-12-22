b055a32c6208871e83162eb09623e551f98aab63
John Friend <goralight@gmail.com>
2023-12-22 14:07:29 +0000

    Fixed black and white not being usable for icon, and updated stories for it

--- src/components/atoms/Icon/Icon.stories.tsx
+++ src/components/atoms/Icon/Icon.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import Icon from '.'
+import { Colors, standardTheme } from '../../../theme'
 
 const meta = {
   title: 'Atoms/Icon',
@@ -14,7 +15,7 @@ const meta = {
     color: 'primary',
     size: 'sm'
   },
-  tags: ['autodocs'],
+  tags: ['autodocs']
 } satisfies Meta<typeof Icon>
 
 export default meta
@@ -22,3 +23,39 @@ type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {}
 Primary.storyName = 'Icon'
+const colors: (keyof Colors | 'white' | 'black')[] = [
+  'white',
+  'black',
+  ...Object.keys(standardTheme.palette.colors) as (keyof typeof standardTheme.palette.colors)[]
+]
+
+const colorIcons = colors.map((color) => (
+  <div key={color} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <h3>{color}</h3>
+    <Icon icon='otter' color={color} size='xxl' />
+  </div>
+))
+
+export const allColors: Story = {
+  render: (): JSX.Element => (
+    <div style={{ display: 'flex', gap: '16px' }}>
+      {colorIcons}
+    </div>
+  )
+}
+allColors.storyName = 'All Colors'
+
+const sizes: (keyof typeof standardTheme.spacing)[] = Object.keys(standardTheme.spacing) as (keyof typeof standardTheme.spacing)[]
+const sizeIcons = sizes.map((size) => (
+  <div key={size} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <h3>{size}</h3>
+    <Icon icon='otter' size={size} />
+  </div>
+))
+export const allSizes: Story = {
+  render: (): JSX.Element => (
+    <div style={{ display: 'flex', gap: '16px' }}>
+      {sizeIcons}
+    </div>
+  )
+}
--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -7,25 +7,60 @@ import styled from '@emotion/styled'
 
 interface IconProps {
   icon: IconProp
-  color: keyof Colors
-  size: keyof ExtraSizing
+  color?: keyof Colors | 'white' | 'black'
+  size?: keyof ExtraSizing,
+  onClick?: () => void
+}
+interface StyledFontAwesomeIconProps {
+  color: keyof Colors | 'white' | 'black'
+  width: keyof ExtraSizing
+  height: keyof ExtraSizing
+  isClickable: boolean
 }
 
-const StyledFontAwesomeIcon = styled(FontAwesomeIcon)<{ color: keyof Colors, width: keyof ExtraSizing, height: keyof ExtraSizing }>(
-  ({ theme, color, width, height }): string => {
-    const iconColor = theme.palette.colors[color].main // todo: fix that we cant black / white
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)<StyledFontAwesomeIconProps>(
+  ({ theme, color, width, height, isClickable }): string => {
+    let iconColor: string
+    if (color === 'white' || color === 'black') {
+      iconColor = theme.palette.colors.grey[color]
+    } else {
+      iconColor = theme.palette.colors[color].main
+    }
+
+    let hoverColor: string
+    if (color === 'white') {
+      hoverColor = theme.palette.colors.grey.light
+    } else if (color === 'black') {
+      hoverColor = theme.palette.colors.grey.dark
+    } else {
+      hoverColor = theme.palette.colors[color].dark
+    }
 
     return `
       color: ${iconColor};
       width: ${theme.spacing[width]}px;
       height: ${theme.spacing[height]}px;
+      cursor: ${isClickable ? 'pointer' : 'default'};
+
+      ${isClickable ? `
+        transition: color 0.2s ease;
+
+        &:hover {
+          color: ${hoverColor};
+        }
+      ` : null}
     `
   }
 )
 
-const Icon = ({ icon, color, size }: IconProps): JSX.Element => {
+const Icon = ({
+  icon,
+  color = 'primary',
+  size = 'md',
+  onClick
+}: IconProps): JSX.Element => {
   return (
-    <StyledFontAwesomeIcon icon={icon} color={color} height={size} width={size} />
+    <StyledFontAwesomeIcon icon={icon} color={color} height={size} width={size} isClickable={!!onClick} onClick={onClick} />
   )
 }
 export default Icon
